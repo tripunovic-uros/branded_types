4f4e42f7dc5d0652e69b3bdad9efd08839566637
Uros <uros.tripunovic@omegapoint.se>
2025-02-20 21:51:50 +0100

    add branded lsp support

--- domain.ts
+++ domain.ts
@@ -1,8 +1,10 @@
 import { z } from "zod";
 import { DomainError } from "./exempel-bra";
 
-const emailSchema = z.string().email()//.brand<'Email'>();
-type Email = z.infer<typeof emailSchema>// & { readonly __brand: unique symbol };
+const emailSchema = z.string().email().brand<'Email'>();
+type Email = z.infer<typeof emailSchema> & {}
+// & {} tvingar TypeScript att behandla typen på nytt istället för att bara använda z.infer som ett alias.
+// Med & {} säger vi till TypeScript att expandera typen och ge den det namn vi sagt att den skall heta.
 
 let unknown_string = 'hehe@email.com';
 
@@ -14,7 +16,6 @@ const createEmail = (value: unknown): Email => {
 }
 
 const email = createEmail(unknown_string);
-
 
 const echoEmail = (email: Email) => console.log(email);
 
